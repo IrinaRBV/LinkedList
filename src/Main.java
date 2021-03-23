bee70c49f4d5f42bedd6ce4b18e7e03bf092b64c
Irina Ryabova <irina2013266@gmail.com>
2021-03-23 19:40:49 +0300

    Переименование класса Test в Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Test {
+public class Main {
     public static void main(String[] args) {
         MyLinkedList myLinkedList = new MyLinkedList();
 
